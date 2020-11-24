dad6339add8bd0065d5ad2b2479cd74276dcceb9
Robert Peralta <rapito@gmail.com>
2020-11-24 11:34:33 -0400

    Allow custom logger class based on https://github.com/papertrail/remote_syslog_logger/issues/29

--- lib/remote_syslog_logger.rb
+++ lib/remote_syslog_logger.rb
@@ -3,9 +3,10 @@ require 'remote_syslog_logger/udp_sender'
 require 'logger'
 
 module RemoteSyslogLogger
-  VERSION = '1.0.4'
+  VERSION = '1.0.5'
 
   def self.new(remote_hostname, remote_port, options = {})
-    Logger.new(RemoteSyslogLogger::UdpSender.new(remote_hostname, remote_port, options))
+    logger_class = options[:logger_class] || Logger
+    logger_class.new(RemoteSyslogLogger::UdpSender.new(remote_hostname, remote_port, options))
   end
 end
